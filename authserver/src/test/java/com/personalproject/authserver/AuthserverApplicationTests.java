109ef0974480ea941e464483fe54a11995581e6b
김지효B/SGS DevCamp <jihyokim@smilegate.com>
2021-12-15 16:02:24 +0900

    init

---
+++ authserver/src/test/java/com/personalproject/authserver/AuthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalproject.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
